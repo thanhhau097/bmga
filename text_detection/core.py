131488affd91e7ec4d3778e5b6addfbb12b45935
Thanh Hau Nguyen <t.nguyen@sprout.ai>
2023-03-25 07:36:13 +0000

    add db and parseq

---
+++ text_detection/core.py
@@ -0,0 +1,45 @@
+import os
+import torch
+import cv2
+import numpy as np
+import math
+
+from .src.demo import Demo, Structure, Experiment, Configurable, Config
+
+
+
+class TextDetectionModel:
+    def __init__(
+            self,
+            weights_path,
+            config_path,
+            image_short_side,
+            thresh,
+            box_thresh,
+            resize,
+            polygon,
+            device="cuda"
+        ):
+        args = {
+            "resume": weights_path,
+            "image_short_side": image_short_side,
+            "thresh": thresh,
+            "box_thresh": box_thresh,
+            "resize": resize,
+            "polygon": polygon
+        }
+
+        conf = Config()
+        experiment_args = conf.compile(conf.load(config_path))['Experiment']
+        experiment_args.update(cmd=args)
+        experiment = Configurable.construct_class_from_config(experiment_args)
+
+        self.predictor = Demo(experiment, experiment_args, cmd=args)
+
+    def predict(self, image_paths, visualize=False):
+        all_outputs = []
+        for path in image_paths:
+            outputs = self.predictor.inference(path, visualize)
+            all_outputs.append(outputs)
+
+        return all_outputs
---
+++ text_recognition/core.py
@@ -0,0 +1,28 @@
+import torch
+from PIL import Image
+from src.strhub.data.module import SceneTextDataModule
+
+
+class TextRecognitionModel:
+    def __init__(self, weights_path='baudm/parseq', model_name="parseq") -> None:
+        # Load model and image transforms
+        self.parseq = torch.hub.load(weights_path, model_name, pretrained=True).eval()
+        self.img_transform = SceneTextDataModule.get_transform(self.parseq.hparams.img_size)
+
+    def predict(self, image_paths):
+        labels, confidences = [], []
+        for path in image_paths:
+            img = Image.open(path).convert('RGB')
+            # Preprocess. Model expects a batch of images with shape: (B, C, H, W)
+            img = self.img_transform(img).unsqueeze(0)
+
+            logits = self.parseq(img)
+            logits.shape  # torch.Size([1, 26, 95]), 94 characters + [EOS] symbol
+
+            # Greedy decoding
+            pred = logits.softmax(-1)
+            label, confidence = self.parseq.tokenizer.decode(pred)
+            labels.append(label)
+            confidences.append(confidence)
+
+        return labels, confidences
